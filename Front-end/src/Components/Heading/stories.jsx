ea45f6461acfe1c8dccb1adf09b2e06a6bc5cd27
Yuran Simao <yuransimao7@gmail.com>
2023-07-17 21:56:56 +0100

    update

--- Front-end/src/Components/Heading/stories.jsx
+++ Front-end/src/Components/Heading/stories.jsx
@@ -3,6 +3,9 @@ import {Heading} from"."
 export default {
     tittle: "Heading",
     component: Heading,
+    args :{
+        children : "Nao sei"
+    }
 }
 
 export const Template = (args) => <Heading {...args}/>

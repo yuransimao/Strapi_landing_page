ba756545440e23b1099be2dc4a73b25a64735976
Yuran Simao <yuransimao7@gmail.com>
2023-07-17 16:54:48 +0100

    configurando Styled-components

--- Front-end/src/main.jsx
+++ Front-end/src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App/App.jsx'
-
+import {GlobalStyle} from "./Styles/global_styles.js"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <GlobalStyle/>
     <App />
   </React.StrictMode>,
 )
